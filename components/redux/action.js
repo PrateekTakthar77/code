9a17a1f79e70a43cae4869e48b4edd57c073dba8
Harsh K. Lamba <harsh.k.lamba@iongroup.com>
2023-06-29 23:54:51 +0530

    fixed error

--- components/redux/action.js
+++ components/redux/action.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, USER_LIST } from './constants';
+import { ADD_TO_CART, REMOVE_FROM_CART, USER_LIST, SET_ACTIVE_ITEM } from './constants';
 
 export function addToCart(item) {
     return {
