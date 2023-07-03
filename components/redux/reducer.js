aa9f018b96d39fd0a3055798a784aee9f5c90316
Harsh K. Lamba <harsh.k.lamba@iongroup.com>
2023-07-04 00:10:40 +0530

    added console

--- components/redux/reducer.js
+++ components/redux/reducer.js
@@ -87,8 +87,9 @@ export const reducer = (state = initialState, action = {}) => {
     }
 };
 
-function calculateCartTotal(cartItems, initialTotal) {
+function calculateCartTotal(cartItems, initialTotal = 0) {
     return cartItems.reduce((acc, cartItem) => {
+        console.log(acc, cartItem);
         return acc + parseInt(cartItem.item.price) * parseInt(cartItem.item.count);
     }, initialTotal)
 }
